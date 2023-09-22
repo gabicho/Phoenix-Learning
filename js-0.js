2b54ad851ceddbb4dae2eebb320605d8d782e5f1
Rodrigo_2 <Rodrigo_2@DESKTOP-2ADRJ2M>
2023-09-22 17:26:07 -0500

    Ejercicio js-0

---
+++ js-0.js
@@ -0,0 +1,40 @@
+//* Crea variables siguiendo las reglas de nomenclatura adecuadas.
+//*****************************************************************/
+
+//*Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
+let nombre = "Juan Gabriel";
+let apellido = "Rodriguez Camma";
+let direccion = "Dirección del domicilio";
+let departamento = "Arequipa";
+let provincia = "Arequipa";
+let distrito = "Cayma";
+let color ="rojo";
+let observacion ="descripción de observación";
+let producto ="licores";
+let detalleProducto ="Tabernero de 500";
+
+//*Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let numero = 10;
+let cantidad = 15;
+let edad = 27;
+let numeroHijos = 3;
+let numeroAccidentes = 5;
+let cantidadSiniestros = 5;
+let paticipaciones =20;
+let numeroAsistencias = 15;
+let numeroHermanos = 3;
+let numeroCajas = 16;
+
+
+//* Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+
+let utilizaServicio = true;
+let isCasado = true;
+let esVehiculo = true;
+let tieneLicencia = false;
+let esComercial = false;
+let esNuevo = true;
+let tieneHijos= true;
+let esCasado = false;
+let tieneHijos = true;
+let tieneDescuento = true;
